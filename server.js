4457e23adcbd62963102fcb0e4599abb525cb675
DouglasMV <douglasmv@ymail.com>
2019-11-15 21:04:02 -0300

    chat finished

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const io = require('socket.io')(8000)
 
-io.on('conection', socket => {
-  socket.emit('chat-msg', 'Hello World')
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-msg', msg => {
+    socket.broadcast.emit('chat-msg', { msg, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
